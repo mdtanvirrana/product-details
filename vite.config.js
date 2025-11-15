0004545e43e209374181eaa1884264da98f971ba
tanvir-rana09 <tanvir.rana.soikat@gmail.com>
2025-11-15 23:15:50 +0600

    add

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    content: [
-        "index.html",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Open Sans', 'sans-serif'],
-            },
-        },
-    },
     plugins: [
         tailwindcss(),
     ],
